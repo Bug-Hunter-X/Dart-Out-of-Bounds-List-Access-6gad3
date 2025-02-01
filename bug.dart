f46284b9f63273895d982e4d20dfd39ebfaf2694
BugHunter-X <askzenai@gmail.com>
2025-02-01 05:16:10 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,7 @@
+```dart
+List<int> numbers = [1, 2, 3, 4, 5];
+
+// Try to access an index that is out of bounds
+int index = numbers.length; 
+int value = numbers[index]; // This will throw an error
+```
